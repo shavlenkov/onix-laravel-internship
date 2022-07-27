d163da20ef8fcc30a2133fda819145d3cd28ec96
shavlenkov <pashashavlenkov@gmail.com>
2022-07-27 21:41:33 +0300

    [fix] removed  variable

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -14,9 +14,7 @@ class PostController extends Controller
      */
     public function index()
     {
-        $posts = Post::all();
-
-        return $posts;
+        return Post::all();
     }
 
     /**
