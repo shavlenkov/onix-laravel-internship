d481b7552ae700580a58412d76af4c1e9be7dd36
shavlenkov <pashashavlenkov@gmail.com>
2022-08-05 23:31:08 +0300

    [fix] updated validation

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use App\Http\Requests\UpdateUserRequest;
+
 use Illuminate\Http\Request;
 
 class UserController extends Controller
@@ -9,5 +12,22 @@ class UserController extends Controller
     public function profile()
     {
         return auth()->user();
+    }
+
+    public function update(UpdateUserRequest $request, User $user)
+    {
+        $data = $request->validated();
+
+        $data['password'] = bcrypt($data['password']);
+        $data['password_confirmation'] = bcrypt($data['password_confirmation']);
+
+        $user->name = $data['name'];
+        $user->email = $data['email'];
+        $user->password = $data['password'];
+
+        $user->save();
+
+        return response()
+            ->json(['success' => true]);
     }
 }
