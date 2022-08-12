fd68542d9cfbeec1846a4c83fc74220502064c78
shavlenkov <pashashavlenkov@gmail.com>
2022-08-13 02:12:51 +0300

    [feat] en lang

---
+++ resources/lang/en/messages.php
@@ -0,0 +1,27 @@
+<?php
+
+return [
+    'lang' => 'en',
+
+    'signin.title' => 'Sign In',
+    'signup.title' => 'Sign Up',
+
+    'auth.label.name' => 'Name',
+    'auth.label.email' => 'Email',
+    'auth.label.password' => 'Password',
+
+    'signup.btn' => 'Sign Up',
+    'signin.btn' => 'Sign In to your profile',
+
+    'posts.index.title' => 'All posts',
+    'posts.create.title' => 'Create post',
+    'posts.edit.title' => 'Edit post',
+
+    'posts.create.btn' => 'Create',
+    'posts.edit.btn' => 'Edit',
+    'posts.show.btn' => 'Show',
+
+    'posts.placeholder.title' => 'Post title',
+    'posts.placeholder.text' => 'Post text',
+    'posts.placeholder.keywords' => 'Keywords',
+];
