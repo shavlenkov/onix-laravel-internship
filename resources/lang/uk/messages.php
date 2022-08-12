57d458005741f77939dc1100a7866177ee75e72e
shavlenkov <pashashavlenkov@gmail.com>
2022-08-13 02:13:11 +0300

    [feat] uk lang

---
+++ resources/lang/uk/messages.php
@@ -0,0 +1,27 @@
+<?php
+
+return [
+    'lang' => 'uk',
+
+    'signin.title' => 'Вхід',
+    'signup.title' => 'Реєстрація',
+
+    'auth.label.name' => "Ім'я",
+    'auth.label.email' => 'Електронна пошта',
+    'auth.label.password' => 'Пароль',
+
+    'signin.btn' => 'Увійти в профіль',
+    'signup.btn' => 'Зареєструватись',
+
+    'posts.index.title' => 'Усі пости',
+    'posts.create.title' => 'Створити пост',
+    'posts.edit.title' => 'Відредагувати пост',
+
+    'posts.create.btn' => 'Створити',
+    'posts.edit.btn' => 'Відредагувати',
+    'posts.show.btn' => 'Переглянути',
+
+    'posts.placeholder.title' => 'Назва посту',
+    'posts.placeholder.text' => 'Текст посту',
+    'posts.placeholder.keywords' => 'Ключові слова',
+];
