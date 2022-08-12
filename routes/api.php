b1650e899e987b681152398fa7f357c31ec62bfe
shavlenkov <pashashavlenkov@gmail.com>
2022-08-13 02:48:19 +0300

    [feat] added 'api' prefix to route name

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-use App\Http\Controllers\PostController;
+use App\Http\Controllers\Api\PostController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,4 +20,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::apiResource('posts', PostController::class);
+Route::apiResource('posts', PostController::class, [
+    'as' => 'api'
+]);
