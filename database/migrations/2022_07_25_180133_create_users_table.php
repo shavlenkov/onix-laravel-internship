a914729e25a600debc7b7ad290d0f3051d2bc036
shavlenkov <pashashavlenkov@gmail.com>
2022-08-13 02:44:01 +0300

    [feat] added bigIncrements

--- database/migrations/2022_07_25_180133_create_users_table.php
+++ database/migrations/2022_07_25_180133_create_users_table.php
@@ -6,7 +6,6 @@ use Illuminate\Support\Facades\Schema;
 
 class CreateUsersTable extends Migration
 {
-
     /**
      * Run the migrations.
      *
@@ -15,7 +14,7 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
+            $table->bigIncrements('id');
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
