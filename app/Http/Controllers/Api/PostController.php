f548d0b633b3198d2d225a7deee97970818b98a5
shavlenkov <pashashavlenkov@gmail.com>
2022-08-13 01:52:46 +0300

    [feat] Api controller

---
+++ app/Http/Controllers/Api/PostController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+
+use App\Models\Post;
+use App\Http\Requests\StoreUpdatePostRequest;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Post::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreUpdatePostRequest $request)
+    {
+        $post = Post::create($request->all());
+
+        return response()
+            ->json(['success' => true]);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Post $post)
+    {
+        return $post;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function update(StoreUpdatePostRequest $request, Post $post)
+    {
+        $post->title = $request->input('title');
+        $post->keywords = $request->input('keywords');
+        $post->text = $request->input('text');
+
+        $post->save();
+
+        return response()
+            ->json(['success' => true]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Post $post)
+    {
+        $post->delete();
+
+        return response()
+            ->json(['success' => true]);
+    }
+}
