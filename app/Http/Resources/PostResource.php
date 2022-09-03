0fb87fafe28a669d3a05df19f516756fe5b2734a
shavlenkov <pashashavlenkov@gmail.com>
2022-09-03 20:49:21 +0300

    [feat] PostResource

--- app/Http/Resources/PostResource.php
+++ app/Http/Resources/PostResource.php
@@ -15,9 +15,14 @@ class PostResource extends JsonResource
     public function toArray($request)
     {
         return [
+            'id' => $this->resource->id,
             'title' => $this->resource->title,
             'text' => $this->resource->text,
             'userId' => $this->resource->userId,
+            'author' => $this->resource->author,
+            'keywords' => $this->resource->keywords,
+            'created_at' => $this->resource->created_at,
+            'updated_at' => $this->resource->updated_at
         ];
     }
 }
