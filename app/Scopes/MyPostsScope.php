2c81c51846b4262e2bf29696f758b7dab2d02abe
shavlenkov <pashashavlenkov@gmail.com>
2022-09-04 21:20:47 +0300

    [feat] Scopes

---
+++ app/Scopes/MyPostsScope.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Scopes;
+
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Scope;
+
+use Auth;
+
+class MyPostsScope implements Scope
+{
+    /**
+     * Apply the scope to a given Eloquent query builder.
+     *
+     * @param Builder $builder
+     * @param Model $model
+     * @return void
+     */
+    public function apply(Builder $builder, Model $model)
+    {
+        $builder->where('userId', Auth::user()->id);
+    }
+}
