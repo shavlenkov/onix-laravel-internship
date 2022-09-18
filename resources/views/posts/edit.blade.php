fc1900759e7efb8ef5a6b0632d0485f6106bbbd6
shavlenkov <pashashavlenkov@gmail.com>
2022-09-18 23:01:43 +0300

    [feat] added relationship

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -11,7 +11,7 @@
                 @method('PUT')
                 <input name="title" value="{{ $post->title }}" type="text" class="form-control" placeholder="{{ __('messages.posts.placeholder.title') }}"><br>
                 <textarea class="form-control" placeholder="{{ __('messages.posts.placeholder.text') }}" style="resize: none" name="text" id="" cols="50" rows="10">{{ $post->text }}</textarea><br>
-                <input name="keywords" value="{{ $post->keywords }}" type="text" class="form-control" placeholder="{{ __('messages.posts.placeholder.keywords') }}"><br>
+                <input name="keywords" value="{{ $post->tags()->first()->name }}" type="text" class="form-control" placeholder="{{ __('messages.posts.placeholder.keywords') }}"><br>
                 <input name="cover" type="file" class="form-control"><br>
 
                 <button type="submit" class="btn btn-primary">{{ __('messages.posts.edit.btn') }}</button>
