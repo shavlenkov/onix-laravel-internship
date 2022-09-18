07b5fa35061dee03048fadf7b34cf2bfe96073f1
shavlenkov <pashashavlenkov@gmail.com>
2022-09-18 22:16:35 +0300

    [feat] updated fields

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -61,7 +61,8 @@ class UserController extends Controller
         $data = $request->validated();
         $data['password'] = bcrypt($data['password']);
 
-        $user->name = $data['name'];
+        $user->first_name = $data['first_name'];
+        $user->last_name = $data['last_name'];
         $user->email = $data['email'];
         $user->password = $data['password'];
 
