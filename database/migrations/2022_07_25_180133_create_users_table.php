facf18e2c631bbfbd3a64212636014cde2d8906c
shavlenkov <pashashavlenkov@gmail.com>
2022-09-18 22:49:45 +0300

    [feat] added fiels: first_name, last_name, full_name, total_posts

--- database/migrations/2022_07_25_180133_create_users_table.php
+++ database/migrations/2022_07_25_180133_create_users_table.php
@@ -15,9 +15,12 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('full_name');
             $table->string('email')->unique();
             $table->string('password');
+            $table->bigInteger('total_posts')->unsigned()->default(0);
             $table->timestamps();
         });
     }
