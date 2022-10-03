f71d7a0f7949ff51688f5af15a731024b8dd105c
shavlenkov <pashashavlenkov@gmail.com>
2022-10-03 22:51:13 +0300

    [feat] added img tag (for cover)

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -4,6 +4,10 @@
 
 @section('content')
     <div class="row">
+        @if($post->cover != "")
+            <img src="{{ Storage::url($post->cover) }}"/>
+        @endif
+
         <h3>{{ $post->title }}</h3>
         <p>{{ $post->text }}</p>
         <p>{{ $post->keywords }}</p>
