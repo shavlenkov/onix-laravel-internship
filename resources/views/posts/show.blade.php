f0519172bcd034a7e08f935cde6324cbe83e77fd
shavlenkov <pashashavlenkov@gmail.com>
2022-10-05 22:43:32 +0300

    [fix] added $post->tags()->first()->name

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -4,12 +4,13 @@
 
 @section('content')
     <div class="row">
-        @if($post->cover != "")
+        @if(!empty($post->cover))
             <img src="{{ Storage::url($post->cover) }}"/>
         @endif
 
         <h3>{{ $post->title }}</h3>
         <p>{{ $post->text }}</p>
-        <p>{{ $post->keywords }}</p>
+        <hr/>
+        <p>{{ $post->tags()->first()->name}}</p>
     </div>
 @endsection
