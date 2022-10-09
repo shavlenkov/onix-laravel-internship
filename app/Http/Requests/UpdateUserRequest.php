1e7849224243800873e0fc3ce117ee0210e488f2
Павел Шавленков <69006300+shavlenkov@users.noreply.github.com>
2022-10-09 23:09:03 +0300

    [fix] fixed the validation rule

--- app/Http/Requests/UpdateUserRequest.php
+++ app/Http/Requests/UpdateUserRequest.php
@@ -18,7 +18,7 @@ class UpdateUserRequest extends FormRequest
         return [
             'first_name' => 'required',
             'last_name' => 'required',
-            'email' => 'required|unique:users|email:rfc,dns',
+            'email' => 'required|email:rfc,dns|unique:users,email,'.auth()->user()->id,
             'password' => ['required', 'confirmed', Password::min(6)->mixedCase()->numbers()],
             'password_confirmation' => 'requiredWith:password',
         ];
